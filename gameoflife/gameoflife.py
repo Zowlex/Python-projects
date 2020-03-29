964831e646e9a93fe056d4162f64fcba385f4cad
Fares Lassoued <fareslass@gmail.com>
2020-03-29 15:48:51 +0100

    version 1.0 added drawing capability

--- gameoflife/gameoflife.py
+++ gameoflife/gameoflife.py
@@ -4,100 +4,119 @@ from grid import Grid
 from pygame.locals import *
 import time
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+def main():
+    # Define some colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
 
-# This sets the WIDTH and HEIGHT of each grid location
-SQUARE = 20
-
-# This sets the margin between each cell
-MARGIN = 2
- 
-def create_window(w, h):
-    # Set the HEIGHT and WIDTH of the screen
-    screen = pygame.display.set_mode([w,h])
-    # Set title of screen
-    pygame.display.set_caption("John Conway's game of life")
-    return screen
-
-# Initialize pygame
-pygame.init()
-
-#create window
-w = 700
-h = 500
-screen = create_window(w, h)
-
-# Loop until the user clicks the close button.
-done = False
+   
     
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+    def create_window(w, h):
+        # Set the HEIGHT and WIDTH of the screen
+        screen = pygame.display.set_mode([w,h])
+        # Set title of screen
+        pygame.display.set_caption("John Conway's game of life")
+        return screen
 
-#calculate row_num, col_num from current resolution
-row_num = w//SQUARE
-col_num = h//SQUARE
-#create grid
-grid = Grid(row_num, col_num)
-print(grid.shape())
-grid.randomize()
-"""
-#Try the blinker oscilator
-grid[1][2] = 1
-grid[2][2] = 1
-grid[3][2] = 1
-"""
-#def initialize grid
-def draw_grid(row_num, col_num, g):
-                # Draw the grid
-                for row in range(row_num):
-                    for column in range(col_num):
-                        color = WHITE
-                        pygame.draw.rect(screen,
-                                            color,
-                                            [(MARGIN + SQUARE) * row + MARGIN,
-                                            (MARGIN + SQUARE) * column + MARGIN,
-                                            SQUARE,
-                                            SQUARE])
-                        if g[row][column] == 1:
+    # Initialize pygame
+    pygame.init()
+
+    # This sets the WIDTH and HEIGHT of each grid location
+    SQUARE = 50
+
+    # This sets the margin between each cell
+    MARGIN = 2
+
+    #create window
+    w = 500
+    h = 500
+    screen = create_window(w, h)
+
+    # Loop until the user clicks the close button.
+    done = False
+        
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+
+    #calculate row_num, col_num from current resolution
+    row_num = w//SQUARE
+    col_num = h//SQUARE
+    print(row_num, col_num)
+    #create grid
+    grid = Grid(row_num, col_num)
+    #grid.randomize()
+    """
+    #Try the blinker oscilator
+    grid[1][2] = 1
+    grid[2][2] = 1
+    grid[3][2] = 1
+    """
+    #def initialize grid
+    def draw_grid(row_num, col_num, g):
+                    # Draw the grid
+                    for row in range(row_num):
+                        for column in range(col_num):
+                            color = WHITE
                             pygame.draw.rect(screen,
-                                    GREEN,
-                                    [(MARGIN + SQUARE) * row + MARGIN,
-                                    (MARGIN + SQUARE) * column + MARGIN,
-                                    SQUARE,
-                                    SQUARE])
-                pygame.display.flip()
-                
-                        
+                                                color,
+                                                [(MARGIN + SQUARE) * row + MARGIN,
+                                                (MARGIN + SQUARE) * column + MARGIN,
+                                                SQUARE,
+                                                SQUARE])
+                            if g[row][column] == 1:
+                                pygame.draw.rect(screen,
+                                        GREEN,
+                                        [(MARGIN + SQUARE) * row + MARGIN,
+                                        (MARGIN + SQUARE) * column + MARGIN,
+                                        SQUARE,
+                                        SQUARE])
+                    pygame.display.flip()
                     
-#draw initial grid
-draw_grid(row_num, col_num, grid)                        
+                            
+                        
+    #draw initial grid
+    draw_grid(row_num, col_num, grid)                        
 
-# -------- Main Program Loop -----------
-while not done:
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
-        #elif event.type == pygame.VIDEORESIZE: 
-        #    screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
-        #    h,w = event.dict['size']
-            #screen.blit(pygame.transform.scale(pic,event.dict['size']),(0,0))
-        #    pygame.display.flip()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == K_SPACE:
+    # -------- Main Program Loop -----------
+    while not done:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            #elif event.type == pygame.VIDEORESIZE: 
+            #    screen=pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
+            #    h,w = event.dict['size']
+                #screen.blit(pygame.transform.scale(pic,event.dict['size']),(0,0))
+            #    pygame.display.flip()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    
+                    scaled_pos = [x//SQUARE for x in pos]
+                    x, y = scaled_pos[0], scaled_pos[1]
+                    grid[x][y]=1
+                    draw_grid(row_num, col_num, grid)
+                else:
+                    pos = pygame.mouse.get_pos()
+                    scaled_pos = [x//SQUARE for x in pos]
+                    x, y = scaled_pos[0], scaled_pos[1]
+                    grid[x][y]=0
+                    draw_grid(row_num, col_num, grid)
+
+            elif event.type == pygame.KEYDOWN and event.key == K_SPACE:
+                
                 print('space key pressed')
                 x = 0
-                while True:
+                game_started = True
+                while game_started:
                     next_grid = Grid(row_num, col_num)
                     for row in range(row_num):
                         for col in range(col_num):
 
                             state = grid[row][col]#current grid state 0 or 1
                             
-                             
+                            
                             # #ignore edges for the moment --> Already fixed in version 0.8
                             # if row == 0 or row == row_num-1 or col == 0 or col == col_num-1:
                             #     next_grid[row][col] = state
@@ -118,13 +137,18 @@ while not done:
                     x+=1
                     print('generation:',x)
                     # Limit to x frames per second
-                    clock.tick(x)
-                
+                    clock.tick(5)
+                    
+
+
     
 
-    # Go ahead and update the screen with what we've drawn.
-    #pygame.display.update()
+        # Go ahead and update the screen with what we've drawn.
+        #pygame.display.update()
 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
-pygame.quit()
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
